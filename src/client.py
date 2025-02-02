03d9e8c412c5c9cd6088a7e45eb9ea946b28173a
joaov1ct0r <jov9051@outlook.com>
2025-02-01 23:12:48 -0300

    refactor: changed subprocess function to handle data received from server

--- src/client.py
+++ src/client.py
@@ -25,23 +25,22 @@ class Client():
         return f'{output.stdout.read()} {output.stderr.read()}'
     
     def receive(self):
-        while self.KEEP_GOING:
-            try:
-                data = self.s.recv(1024).decode('utf-8')
+        try:
+            data = self.s.recv(1024).decode('utf-8')
 
-                if not data or data == 'q':
-                    self.s.shutdown(1)
-                    self.s.close()
+            if not data or data == 'q':
+                self.s.shutdown(1)
+                self.s.close()
 
-                print(f'Received from server: {data}')
+            print(f'Received from server: {data}')
 
-                output = self.handle(data)
+            output = self.handle(data)
 
-                self.send(output)
-            except:
-                self.print_msg('Failed to receive data from server')
-                self.KEEP_GOING = False
-                exit(1)
+            self.send(output)
+        except:
+            self.print_msg('Failed to receive data from server')
+            self.KEEP_GOING = False
+            exit(1)
 
     def main(self):
         try:
